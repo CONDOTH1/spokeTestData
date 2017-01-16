a65b88da0cea9b9b22c68013916a1e495afbb940
Thady Condon <thadycondon@Thadys-iMac.local>
2017-01-16 15:49:34 +0000

    Brought the vars into server

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var http = require('http');
 var url = require('url');
 var person = require('./lib/person');
-var body = [];
-var profiles = [];
 var iterator = 0;
 var port = process.env.PORT || 8080;
 
@@ -10,34 +8,36 @@ var port = process.env.PORT || 8080;
 this.server = http.createServer(function(req, res) {
 
   var parsedURL = url.parse(req.url);
+  var body = [];
+  var profiles = [];
 
 
   var parseRequest = function(body){
     var number = Buffer.concat(body).toString();
     var strNmb = number.replace('profiles=', '');
-    return strNmb - 0
-  }
+    return strNmb - 0;
+  };
 
   var recordPostData = function(){
     req.on('error', function(err) {console.error(err)});
     req.on('data', function(chunk) {body.push(chunk)});
-  }
+  };
 
   var iterateAndSend = function(){
     req.on('end', function() {
-      var numberOfProfiles = parseRequest(body)
+      var numberOfProfiles = parseRequest(body);
       getUserProfiles(numberOfProfiles).then(function(){
         sendResponse(res, req.headers, req.method, req.url);
       });
     });
-  }
+  };
 
   var sendResponse = function(res, headers, method, url){
     res.on('error', function(err) {console.error(err);});
     res.writeHead(200, {'Content-Type': 'application/json'});
     var resBody = {body: profiles, headers: headers, method: method, url:url};
     res.end(JSON.stringify(resBody));
-  }
+  };
 
   var getUserProfiles = function(n){
     return new Promise(function(resolve, reject){
@@ -50,7 +50,7 @@ this.server = http.createServer(function(req, res) {
         });
       }
     });
-  }
+  };
 
   if (parsedURL.pathname === '/' && req.method === 'POST') {
     recordPostData();
